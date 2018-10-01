556f90ac84ff1d7df1743ffb9a5dc60d6cc564fc
Kirill Karmakulov <karmakulov@improveitgroup.com>
2018-10-01 12:18:25 +0700

    Init DB script corrections

--- src/main/resources/init.db.sql
+++ src/main/resources/init.db.sql
@@ -1,4 +1,7 @@
-CREATE ROLE admin WITH LOGIN PASSWORD 'MsgAdmin';
+CREATE ROLE site_msg_application WITH LOGIN PASSWORD 'site_msg_pwd';
 DROP DATABASE IF EXISTS site_messages;
 CREATE DATABASE site_messages;
-GRANT ALL PRIVILEGES ON DATABASE site_messages TO admin;
+GRANT ALL PRIVILEGES ON DATABASE site_messages TO site_msg_application;
+
+\c site_messages;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA pg_catalog;
